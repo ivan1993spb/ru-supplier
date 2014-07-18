d2493c350fca002f4e101814be04e677eda4306f
Ivan Pushkin <pushkin13@bk.ru>
2014-07-19 00:31:28 +0400

    windows

---
+++ strings.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	_PROGRAM_TITLE                     = "Внимательный Поставшик"
+	_TAB_TITLE_SERVER_SETTINGS         = "Прокси"
+	_TAB_TOOL_TIP_TEXT_SERVER_SETTINGS = "Настройки локального прокси сервера"
+	_TAB_TITLE_LINKS                   = "Адреса"
+	_TAB_TOOL_TIP_TEXT_LINKS           = "Генерация ссылок для RSS-клиента"
+)
