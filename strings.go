e812ef529d5ddf2b45c74b3f6edc6372a966eb00
Ivan Pushkin <pushkin13@bk.ru>
2014-07-22 22:15:16 +0400

    created notify icon and context menu

--- strings.go
+++ strings.go
@@ -1,9 +1,13 @@
 package main
 
 const (
-	_PROGRAM_TITLE                     = "Внимательный Поставшик"
-	_TAB_TITLE_SERVER_SETTINGS         = "Прокси"
-	_TAB_TOOL_TIP_TEXT_SERVER_SETTINGS = "Настройки локального прокси сервера"
-	_TAB_TITLE_LINKS                   = "Адреса"
-	_TAB_TOOL_TIP_TEXT_LINKS           = "Генерация ссылок для RSS-клиента"
+	_PROG_TITLE               = "Внимательный Поставщик"
+	_PROG_VERSION             = "2.0"
+	_NOTIFY_ICON_TOOL_TIP_MSG = _PROG_TITLE + " " + _PROG_VERSION
+
+// _PROGRAM_TITLE                     = "Внимательный Поставшик"
+// _TAB_TITLE_SERVER_SETTINGS         = "Прокси"
+// _TAB_TOOL_TIP_TEXT_SERVER_SETTINGS = "Настройки локального прокси сервера"
+// _TAB_TITLE_LINKS                   = "Адреса"
+// _TAB_TOOL_TIP_TEXT_LINKS           = "Генерация ссылок для RSS-клиента"
 )
--- windows.go
+++ windows.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/walk"
+	// . "github.com/lxn/walk/declarative"
 )
+
+const _ICON_FILE_NAME = "eagle.ico"
 
 func InterfaceStart(server *Server, config *Config, filter *Filter) error {
 	if server == nil {
@@ -14,7 +17,50 @@ func InterfaceStart(server *Server, config *Config, filter *Filter) error {
 	if filter == nil {
 		panic("interface error: passed nil filter")
 	}
+	mw, err := walk.NewMainWindow()
+	if err != nil {
+		return err
+	}
+	icon, err := walk.NewIconFromFile(_ICON_FILE_NAME)
+	if err != nil {
+		return err
+	}
+	defer icon.Dispose()
+	ni, err := walk.NewNotifyIcon()
+	if err != nil {
+		return err
+	}
+	defer ni.Dispose()
+	if err := ni.SetIcon(icon); err != nil {
+		return err
+	}
+	if err := ni.SetToolTip(_NOTIFY_ICON_TOOL_TIP_MSG); err != nil {
+		return err
+	}
+	context_menu := map[string]walk.EventHandler{
+		"Выход":   func() { walk.App().Exit(0) },
+		"Прокси":  func() {},
+		"Адреса":  func() {},
+		"Фильтры": func() {},
+	}
+	for text, handler := range context_menu {
+		action := walk.NewAction()
+		if err := action.SetText(text); err != nil {
+			return err
+		}
+		action.Triggered().Attach(handler)
+		if err := ni.ContextMenu().Actions().Add(action); err != nil {
+			return err
+		}
+	}
 
-	// ...
-
+	if err := ni.SetVisible(true); err != nil {
+		return err
+	}
+	if err := ni.ShowInfo("Walk NotifyIcon Example", "Click the icon to show again."); err != nil {
+		return err
+	}
+	// Run the message loop.
+	mw.Run()
+	return nil
 }
