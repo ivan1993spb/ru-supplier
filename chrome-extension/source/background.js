72cd09d19903a86f3b45688451d1e638e5ab5b26
Ivan Pushkin <pushkin13@bk.ru>
2014-08-17 22:25:12 +0400

    chrome extension

---
+++ chrome-extension/source/background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	chrome.pageAction.show(tabId);
+	// chrome.pageAction.setTitle({
+	//     tabId: tab.id,
+	//     title: 'url=' + tab.url
+	// });
+});
