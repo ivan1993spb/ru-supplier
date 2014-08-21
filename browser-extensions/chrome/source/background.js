51b88f750e4eb4dfd507deddf0f9ce286c451b3a
Ivan Pushkin <pushkin13@bk.ru>
2014-08-21 22:17:16 +0400

    fx chrome extension

--- browser-extensions/chrome/source/background.js
+++ browser-extensions/chrome/source/background.js
@@ -1,7 +1,24 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	chrome.pageAction.show(tabId);
-	// chrome.pageAction.setTitle({
-	//     tabId: tab.id,
-	//     title: 'url=' + tab.url
-	// });
+
+/**
+ * 
+ * Show pageAction icon when was loaded search page on zakupki.gov.ru
+ * 
+ */
+chrome.webNavigation.onCommitted.addListener(function(e) {
+	chrome.pageAction.show(e.tabId);
+}, {
+	url: [
+		{
+			hostSuffix: 'zakupki.gov.ru',
+			pathEquals: '/epz/order/extendedsearch/search.html'
+		},
+		{
+			hostSuffix: 'zakupki.gov.ru',
+			pathEquals: '/epz/order/quicksearch/search.html'
+		},
+		{
+			hostSuffix: 'zakupki.gov.ru',
+			pathEquals: '/epz/order/quicksearch/update.html'
+		}
+	]
 });
