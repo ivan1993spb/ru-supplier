609992ff8bca522fed503f60a5a8469403158235
Pushkin Ivan <rokus123@yandex.ru>
2014-08-22 09:40:42 +0400

    prefs2

--- browser-extensions/chrome/source/preferences.js
+++ browser-extensions/chrome/source/preferences.js
@@ -1,64 +1,68 @@
 
 /**
  * This file contains default preferences, and
- * tools for preferences editing
+ * tools for editing preferences
  */
 
-var preferences = {
+if (chrome.storage != undefined) {
+	var preferences = {
 
-	'default': {
-		'Host': 'proxy-zakupki-gov-ru.local',
-		'Port': 80
-	},
-
-	'validator': {
-		
-		'Host': function(host) {
-			if (typeof host == 'string') {
-				return /^[a-z0-9\-\.]+$/i.test(host);
-			}
-			return false;
+		'default': {
+			'Host': 'proxy-zakupki-gov-ru.local',
+			'Port': 80
 		},
-		
-		'Port': function(port) {
-			if (typeof port == 'number') {
-				if (port % 1 === 0 && port > 0 && port < 65536) {
-					return true;
+
+		'validator': {
+			
+			'Host': function(host) {
+				if (typeof host == 'string') {
+					return /^[a-z0-9\-\.]+$/i.test(host);
 				}
-			}
-			return false;
-		},
-
-	},
-
-	'get': function(callback) {
-		chrome.storage.local.get(this.default, callback);
-	},
-
-	'set': function(prefs, callback) {
-		if (typeof prefs == 'object') {
-			for (var key in prefs) {
-				if (key in this.default) {
-					if (key in this.validator) {
-						if (!this.validator[key](prefs[key])) {
-							delete prefs[key];
-						}
+				return false;
+			},
+			
+			'Port': function(port) {
+				if (typeof port == 'number') {
+					if (port % 1 === 0 && port > 0 && port < 65536) {
+						return true;
 					}
-				} else {
-					delete prefs[key];
 				}
-			}
-			var flag = false;
-			for (var key in prefs) {
-				if (hasOwnProperty.call(prefs, key)) {
-					flag = true;
-					break;
-				}
-			}
-			if (flag) {
-				chrome.storage.local.set(prefs, callback);
-			}
-		}
-	},
+				return false;
+			},
 
-};
+		},
+
+		'get': function(callback) {
+			chrome.storage.local.get(this.default, callback);
+		},
+
+		'set': function(prefs, callback) {
+			if (typeof prefs == 'object') {
+				for (var key in prefs) {
+					if (key in this.default) {
+						if (key in this.validator) {
+							if (!this.validator[key](prefs[key])) {
+								delete prefs[key];
+							}
+						}
+					} else {
+						delete prefs[key];
+					}
+				}
+				var flag = false;
+				for (var key in prefs) {
+					if (hasOwnProperty.call(prefs, key)) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					chrome.storage.local.set(prefs, callback);
+				}
+			}
+		},
+
+	};
+} else {
+	console.log('undefined chrome.storage');
+}
